7e06859877be1c00afde0625eacada75aa413e52
Jasper Chan <jasperchan515@gmail.com>
2026-03-23 21:11:47 -0700

    chore: run nix fmt

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -30,24 +30,28 @@
           kb = "gmmk/pro/rev1/ansi";
           km = "gigahawk";
         };
-        keychron-k3-pro = (build-kb {
-          qmk-fw = inputs.qmk-fw-keychron;
-          kb = "keychron/k3_pro/ansi/rgb";
-          km = "gigahawk";
-          copy-userspace = true;
-        }).overrideAttrs (final: prev: {
-          # HACK: disable factory test code?
-          # They randomly overrode some user keymap funcs???
-          patchPhase = ''
-            ${prev.patchPhase}
+        keychron-k3-pro =
+          (build-kb {
+            qmk-fw = inputs.qmk-fw-keychron;
+            kb = "keychron/k3_pro/ansi/rgb";
+            km = "gigahawk";
+            copy-userspace = true;
+          }).overrideAttrs
+            (
+              final: prev: {
+                # HACK: disable factory test code?
+                # They randomly overrode some user keymap funcs???
+                patchPhase = ''
+                  ${prev.patchPhase}
 
-            substituteInPlace qmk-fw/keyboards/keychron/bluetooth/factory_test.c \
-              --replace-fail 'bool dip_switch_update_user' \
-                '__attribute__((weak)) bool dip_switch_update_user' \
-              --replace-fail 'bool rgb_matrix_indicators_user' \
-                '__attribute__((weak)) bool rgb_matrix_indicators_user' \
-          '';
-        });
+                  substituteInPlace qmk-fw/keyboards/keychron/bluetooth/factory_test.c \
+                    --replace-fail 'bool dip_switch_update_user' \
+                      '__attribute__((weak)) bool dip_switch_update_user' \
+                    --replace-fail 'bool rgb_matrix_indicators_user' \
+                      '__attribute__((weak)) bool rgb_matrix_indicators_user' \
+                '';
+              }
+            );
       };
     };
 }
